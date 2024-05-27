3427e78d1179e5101882bbf3ab6f7fbf1863ffc1
yahya-m2000 <yahyaimohamed2000@gmail.com>
2024-05-27 11:00:52 +0100

    finding a way to fix this pre-render bug

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,52 +1,62 @@
-"use client";
-import * as React from "react";
-import { Box, Typography } from "@mui/material";
+import React from "react";
 
-import FormModal from "../components/FormModal";
-import SearchBar from "../components/SearchBar";
+type Props = {};
 
-export default function Home() {
-  return (
-    <React.Fragment>
-      <Box
-        component="section"
-        display="flex"
-        flexDirection={"column"}
-        alignItems="center"
-        justifyContent="center"
-        minHeight="100vh"
-      >
-        <Typography
-          variant="h6"
-          textAlign="center"
-          gutterBottom={false}
-          sx={{ fontWeight: "regular", letterSpacing: "0.4rem" }}
-        >
-          NYContra
-        </Typography>
-        <Typography variant="h2" textAlign="center" sx={{ fontWeight: "bold" }}>
-          Content Management System
-        </Typography>
-        <Box
-          component="section"
-          display="flex"
-          flexDirection={"row"}
-          alignItems="center"
-          justifyContent="center"
-          margin={2}
-        >
-          <SearchBar />
-        </Box>
-        <Box
-          component="section"
-          display="flex"
-          flexDirection={"row"}
-          alignItems="center"
-          justifyContent="center"
-        >
-          <FormModal />
-        </Box>
-      </Box>
-    </React.Fragment>
-  );
-}
+const Home = (props: Props) => {
+  return <div>Home</div>;
+};
+
+export default Home;
+
+// "use client";
+// import * as React from "react";
+// import { Box, Typography } from "@mui/material";
+
+// import FormModal from "../components/FormModal";
+// import SearchBar from "../components/SearchBar";
+
+// export default function Home() {
+//   return (
+//     <React.Fragment>
+//       <Box
+//         component="section"
+//         display="flex"
+//         flexDirection={"column"}
+//         alignItems="center"
+//         justifyContent="center"
+//         minHeight="100vh"
+//       >
+//         <Typography
+//           variant="h6"
+//           textAlign="center"
+//           gutterBottom={false}
+//           sx={{ fontWeight: "regular", letterSpacing: "0.4rem" }}
+//         >
+//           NYContra
+//         </Typography>
+//         <Typography variant="h2" textAlign="center" sx={{ fontWeight: "bold" }}>
+//           Content Management System
+//         </Typography>
+//         <Box
+//           component="section"
+//           display="flex"
+//           flexDirection={"row"}
+//           alignItems="center"
+//           justifyContent="center"
+//           margin={2}
+//         >
+//           <SearchBar />
+//         </Box>
+//         <Box
+//           component="section"
+//           display="flex"
+//           flexDirection={"row"}
+//           alignItems="center"
+//           justifyContent="center"
+//         >
+//           <FormModal />
+//         </Box>
+//       </Box>
+//     </React.Fragment>
+//   );
+// }
